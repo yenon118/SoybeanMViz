dc1b1db3883028740fa31697fb14ede33752c5f3
Yen On Chan <yenon118@gmail.com>
2022-06-21 22:57:55 -0500

    Edited CNVR search by chrom and region

--- viewAllCNVRByChromosomeAndRegion.php
+++ viewAllCNVRByChromosomeAndRegion.php
@@ -26,25 +26,32 @@ $position_end_1 = intval(trim($position_end_1))+1;
 <br />
 <br />
 
-<!-- Query data from database -->
+<!-- Query cnvr accession count data from database -->
 <?php
 
-// Check GRIN accession mapping
-$query_str = "SELECT CNVR.Chromosome, CNVR.Start, CNVR.End, CNVR.Width, CNVR.Strand, AM.SoyKB_Accession AS Accession, CNVR.CN ";
+$query_str = "SELECT CNVR.Chromosome, CNVR.Start, CNVR.End, CNVR.Width, CNVR.Strand, ";
+$query_str = $query_str . "COUNT(IF(CNVR.CN = 'CN0', 1, null)) AS CN0, ";
+$query_str = $query_str . "COUNT(IF(CNVR.CN = 'CN1', 1, null)) AS CN1, ";
+$query_str = $query_str . "COUNT(IF(CNVR.CN = 'CN2', 1, null)) AS CN2, ";
+$query_str = $query_str . "COUNT(IF(CNVR.CN = 'CN3', 1, null)) AS CN3, ";
+$query_str = $query_str . "COUNT(IF(CNVR.CN = 'CN4', 1, null)) AS CN4, ";
+$query_str = $query_str . "COUNT(IF(CNVR.CN = 'CN5', 1, null)) AS CN5, ";
+$query_str = $query_str . "COUNT(IF(CNVR.CN = 'CN6', 1, null)) AS CN6, ";
+$query_str = $query_str . "COUNT(IF(CNVR.CN = 'CN7', 1, null)) AS CN7, ";
+$query_str = $query_str . "COUNT(IF(CNVR.CN = 'CN8', 1, null)) AS CN8 ";
 $query_str = $query_str . "FROM soykb.mViz_Soybean_CNVR AS CNVR ";
-$query_str = $query_str . "LEFT JOIN soykb.mViz_Soybean_Accession_Mapping AS AM ";
-$query_str = $query_str . "ON CNVR.Accession = AM.Accession ";
 $query_str = $query_str . "WHERE (CNVR.Chromosome = '" . $chromosome_1 . "') ";
 $query_str = $query_str . "AND (CNVR.Start BETWEEN " . $position_start_1 . " AND " . $position_end_1 . ") ";
 $query_str = $query_str . "AND (CNVR.End BETWEEN " . $position_start_1 . " AND " . $position_end_1 . ") ";
-$query_str = $query_str . "ORDER BY AM.SoyKB_Accession, CNVR.CN, CNVR.Chromosome, CNVR.Start, CNVR.End; ";
+$query_str = $query_str . "GROUP BY CNVR.Chromosome, CNVR.Start, CNVR.End, CNVR.Width, CNVR.Strand ";
+$query_str = $query_str . "ORDER BY CNVR.Chromosome, CNVR.Start, CNVR.End; ";
 
 $stmt = $PDO->prepare($query_str);
 $stmt->execute();
 $result = $stmt->fetchAll();
 
 if (count($result) > 0) {
-    $result_arr = pdoResultFilter($result);
+    $cnvr_accession_count_result_arr = pdoResultFilter($result);
 } else {
     echo "No data found!!!";
 }
@@ -55,7 +62,61 @@ if (count($result) > 0) {
 <!-- Render the table -->
 <?php
 
-    if (count($result_arr) > 0) {
+    if (isset($cnvr_accession_count_result_arr) && is_array($cnvr_accession_count_result_arr) && !empty($cnvr_accession_count_result_arr) && count($cnvr_accession_count_result_arr) > 0) {
+        echo "<div style='width:auto; height:auto; border:3px solid #000; overflow:scroll; max-height:1000px; display:inline-block;'>";
+        echo "<table style='text-align:center;'>";
+
+        // Table header
+        echo "<tr>";
+        foreach ($cnvr_accession_count_result_arr[0] as $key => $value) {
+            echo "<th style=\"border:1px solid black;\">" . strval($key) . "</th>";
+        }
+        echo "</tr>";
+
+        // Table body
+        for ($i = 0; $i < count($cnvr_accession_count_result_arr); $i++) {
+            $tr_bgcolor = ($i % 2 ? "#FFFFFF" : "#DDFFDD");
+
+            echo "<tr bgcolor=\"" . $tr_bgcolor . "\">";
+            foreach ($cnvr_accession_count_result_arr[$i] as $key => $value) {
+                echo "<td style=\"border:1px solid black;min-width:80px;\">" . $value . "</td>";
+            }
+            echo "</tr>";
+        }
+
+        echo "</table>";
+        echo "</div>";
+
+        echo "<br /><br />";
+    }
+
+?>
+
+
+<!-- Query data from database and render the data -->
+<?php
+
+for ($i = 0; $i < count($cnvr_accession_count_result_arr); $i++) {
+
+    // Check GRIN accession mapping
+    $query_str = "SELECT CNVR.Chromosome, CNVR.Start, CNVR.End, CNVR.Width, CNVR.Strand, AM.SoyKB_Accession AS Accession, CNVR.CN ";
+    $query_str = $query_str . "FROM soykb.mViz_Soybean_CNVR AS CNVR ";
+    $query_str = $query_str . "LEFT JOIN soykb.mViz_Soybean_Accession_Mapping AS AM ";
+    $query_str = $query_str . "ON CNVR.Accession = AM.Accession ";
+    $query_str = $query_str . "WHERE (CNVR.Chromosome = '" . $cnvr_accession_count_result_arr[$i]["Chromosome"] . "') ";
+    $query_str = $query_str . "AND (CNVR.Start BETWEEN " . $cnvr_accession_count_result_arr[$i]["Start"] . " AND " . $cnvr_accession_count_result_arr[$i]["End"] . ") ";
+    $query_str = $query_str . "AND (CNVR.End BETWEEN " . $cnvr_accession_count_result_arr[$i]["Start"] . " AND " . $cnvr_accession_count_result_arr[$i]["End"] . ") ";
+    $query_str = $query_str . "ORDER BY CNVR.CN, AM.SoyKB_Accession; ";
+
+    $stmt = $PDO->prepare($query_str);
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+
+    $result_arr = pdoResultFilter($result);
+
+
+    // Render the data
+    if (isset($result_arr) && is_array($result_arr) && !empty($result_arr) && count($result_arr) > 0) {
         echo "<div style='width:auto; height:auto; border:3px solid #000; overflow:scroll; max-height:1000px; display:inline-block;'>";
         echo "<table style='text-align:center;'>";
 
@@ -67,11 +128,11 @@ if (count($result) > 0) {
         echo "</tr>";
 
         // Table body
-        for ($i = 0; $i < count($result_arr); $i++) {
-            $tr_bgcolor = ($i % 2 ? "#FFFFFF" : "#DDFFDD");
+        for ($j = 0; $j < count($result_arr); $j++) {
+            $tr_bgcolor = ($j % 2 ? "#FFFFFF" : "#DDFFDD");
 
             echo "<tr bgcolor=\"" . $tr_bgcolor . "\">";
-            foreach ($result_arr[$i] as $key => $value) {
+            foreach ($result_arr[$j] as $key => $value) {
                 echo "<td style=\"border:1px solid black;min-width:120px;\">" . $value . "</td>";
             }
             echo "</tr>";
@@ -79,7 +140,11 @@ if (count($result) > 0) {
 
         echo "</table>";
         echo "</div>";
+
+        echo "<br /><br />";
     }
+
+}
 
 ?>
 
